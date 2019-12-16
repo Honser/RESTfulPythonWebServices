6e9bedcfd6d338664682421b18b05dac6e302637
Honser <honser@naver.com>
2019-12-16 22:30:44 +0900

    updated_CH7

--- Django01/games_service/games_service/settings.py
+++ Django01/games_service/games_service/settings.py
@@ -134,6 +134,9 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = { 
     'DEFAULT_PAGINATION_CLASS': 
-    'rest_framework.pagination.LimitOffsetPagination', 
-    'PAGE_SIZE': 4 
+    'games.max_limit_pagination.MaxLimitPagination', 
+    'PAGE_SIZE': 4,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',)
 } 
